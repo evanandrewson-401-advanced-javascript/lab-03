c0009e250f93c4e5adc37e1543d8aaf81613ba99
evanandrewson <evanandrewson@gmail.com>
2019-09-19 10:25:12 -0700

    fixed getAll function and added code to index.js to test functions work as intended

--- index.js
+++ index.js
@@ -2,5 +2,22 @@ const DocumentCollection = require('./lib/document-collection');
 
 const documents = new DocumentCollection('savedObjects');
 
+const object1 = { key1: 'value1' };
+const object2 = { key2: 'value2' };
 
-// write some code to exercise your document collection
+documents.save(object1);
+documents.save(object2);
+
+documents.get(object1.id)
+  .then((result) => {
+    console.log(result);
+  });
+documents.get(object2.id)
+  .then((result) => {
+    console.log(result);
+  });
+
+documents.getAll()
+  .then((result) => {
+    console.log(result);
+  });
