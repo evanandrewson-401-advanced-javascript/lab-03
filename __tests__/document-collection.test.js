17582d71542b3459026efee3dc817a33dd5a59e5
evanandrewson <evanandrewson@gmail.com>
2019-09-19 09:05:46 -0700

    got save function and test working

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -6,7 +6,19 @@ jest.mock('../lib/files.js', () => ({
 
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
+const DocumentCollection = require('../lib/document-collection');
+const documentCollection = new DocumentCollection('folder');
 
 describe('Document Collection', () => {
-  // TODO
+  it('saves file', () => {
+    const object = { key: 'value' };
+    writeFile.mockResolvedValue(object);
+
+    console.log(documentCollection);
+    documentCollection.save(object)
+      .then(() => {
+        expect(writeFile.mock.calls.length).toBe(1);
+        expect(writeFile.mock.calls[0][1]).toBe(JSON.stringify(object));
+      });
+  });
 });
